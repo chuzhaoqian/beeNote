d4bf8a7c6079293668b586cbe61733b0f3329e4a
githubzhaoqian <qian_shuang@173.com>
2016-10-18 15:37:29 +0800

    解析配置 json或者yaml
    解析配置 json或者yaml

--- conf.go
+++ conf.go
@@ -15,11 +15,11 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
+	"encoding/json"	// json 编码包
+	"io/ioutil"	// 有效的i/o方法
+	"os"	//系统函数
 
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"	// 实现Go语言的YAML的支持。
 )
 
 const ConfVer = 0
@@ -77,17 +77,30 @@ var conf struct {
 // loadConfig loads customized configuration.
 func loadConfig() error {
 	foundConf := false
+	// func Open(name string) (file *File, err error)
+	// Open打开一个文件用于读取。
+	// 如果操作成功，返回的文件对象的方法可用于读取数据；
+	// 对应的文件描述符具有O_RDONLY模式。如果出错，错误底层类型是*PathError。
 	f, err := os.Open("bee.json")
 	if err == nil {
+		// func (f *File) Close() error
+		// Close关闭文件f，使文件不能用于读写。它返回可能出现的错误。
 		defer f.Close()
 		ColorLog("[INFO] Detected bee.json\n")
+		// func NewDecoder(r io.Reader) *Decoder
+		// NewDecoder创建一个从r读取并解码json对象的*Decoder，解码器有自己的缓冲，并可能超前读取部分json数据。
 		d := json.NewDecoder(f)
+		// func (dec *Decoder) Decode(v interface{}) error
+		// Decode从输入流读取下一个json编码值并保存在v指向的值里，参见Unmarshal函数的文档获取细节信息。
 		err = d.Decode(&conf)
 		if err != nil {
 			return err
 		}
 		foundConf = true
 	}
+	//func ReadFile(filename string) ([]byte, error)
+	//ReadFile 从filename指定的文件中读取数据并返回文件的内容。
+	//成功的调用返回的err为nil而非EOF。因为本函数定义为读取整个文件，它不会将读取返回的EOF视为应报告的错误。
 	byml, erryml := ioutil.ReadFile("Beefile")
 	if erryml == nil {
 		ColorLog("[INFO] Detected Beefile\n")
@@ -99,6 +112,8 @@ func loadConfig() error {
 	}
 	if !foundConf {
 		// Use default.
+		// func Unmarshal(data []byte, v interface{}) error
+		// Unmarshal函数解析json编码的数据并将结果存入v指向的值。
 		err = json.Unmarshal([]byte(defaultConf), &conf)
 		if err != nil {
 			return err
